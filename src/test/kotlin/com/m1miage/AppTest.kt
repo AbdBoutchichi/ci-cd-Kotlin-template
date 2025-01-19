c2f3741b2c4b0a17ecb640e99f47b6059d4bed5d
Abdelmoughit BOUTCHICHI <abdelmoughitboutchi4@gmail.com>
2025-01-19 19:18:10 +0100

    fix: conflit

---
+++ src/test/kotlin/com/m1miage/AppTest.kt
@@ -0,0 +1,18 @@
+package com.m1miage
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertTrue
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    fun testApp() {
+        assertTrue(true)
+    }
+}
